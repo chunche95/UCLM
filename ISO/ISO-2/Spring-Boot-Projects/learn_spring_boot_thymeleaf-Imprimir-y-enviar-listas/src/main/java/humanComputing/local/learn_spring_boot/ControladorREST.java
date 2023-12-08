bdc10bca18042aac69cf2cc9a280f4454e167e33
chunche95 <37621958+chunche95@users.noreply.github.com>
2023-12-08 16:00:33 +0100

    Inyeccion de variables desde application.properties

--- ISO/ISO-2/Spring-Boot-Projects/learn_spring_boot_thymeleaf-Imprimir-y-enviar-listas/src/main/java/humanComputing/local/learn_spring_boot/ControladorREST.java
+++ ISO/ISO-2/Spring-Boot-Projects/learn_spring_boot_thymeleaf-Imprimir-y-enviar-listas/src/main/java/humanComputing/local/learn_spring_boot/ControladorREST.java
@@ -1,17 +1,24 @@
 package humanComputing.local.learn_spring_boot;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
 @Slf4j // Para poder usar log.info
 public class ControladorREST {
+    @Value("${indice.variable_privada}")
+    private String varprivada;
+    @GetMapping(value = "/")
+    public String comienzo(Model model) {
 
-    @GetMapping("/")
-    public String comienzo() {
+        String variablehola = "Hola, Estamos mandando mensajes desde ControladorREST.java";
         log.info("Ejecutando el controlador MVC");
+        model.addAttribute("variable", variablehola);
+        model.addAttribute("variable_privada", varprivada);
         return "indice";
     }
 
