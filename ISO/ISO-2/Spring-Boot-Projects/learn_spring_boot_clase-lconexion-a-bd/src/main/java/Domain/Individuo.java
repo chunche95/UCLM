b22e516a5c9131cd238a8549a1675b317590769b
chunche95 <37621958+chunche95@users.noreply.github.com>
2023-12-08 18:48:15 +0100

    Configuración de conexión a BD con MVC en Spring Boot

---
+++ ISO/ISO-2/Spring-Boot-Projects/learn_spring_boot_clase-lconexion-a-bd/src/main/java/Domain/Individuo.java
@@ -0,0 +1,24 @@
+package Domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+@Entity
+@Table(name = "usuarios")
+public class Individuo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_persona;
+    private String nombre;
+    private String apellidos;
+    private int edad;
+    private int telefono;
+    private String email;
+    private String password;
+}
