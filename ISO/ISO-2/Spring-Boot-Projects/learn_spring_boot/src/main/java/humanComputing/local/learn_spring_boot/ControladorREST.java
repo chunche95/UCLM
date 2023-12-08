71e5230ed9eb0797a6c54172af97e175e5b273a0
chunche95 <37621958+chunche95@users.noreply.github.com>
2023-12-08 13:09:39 +0100

    Añadido log.info a SpringBoot + cambio de puerto en  application.properties

--- ISO/ISO-2/Spring-Boot-Projects/learn_spring_boot/src/main/java/humanComputing/local/learn_spring_boot/ControladorREST.java
+++ ISO/ISO-2/Spring-Boot-Projects/learn_spring_boot/src/main/java/humanComputing/local/learn_spring_boot/ControladorREST.java
@@ -1,13 +1,17 @@
 package humanComputing.local.learn_spring_boot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j // Para poder usar log.info
 public class ControladorREST {
 
     @GetMapping("/")
     public String comienzo() {
+        log.info("Ejecutando el controlador REST");
+        log.debug("Modo debug - +info");
         return "Hola Mundo en Spring";
     }
 
