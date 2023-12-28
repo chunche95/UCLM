3871c22467ecba6942f09ed87add1850d1203a69
chunche95 <37621958+chunche95@users.noreply.github.com>
2023-12-28 20:38:04 +0100

    Seminario 2

---
+++ ipo/2/Seminario2_GestionEventos/Seminario2_GestionEventos/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Seminario2_GestionEventos
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
